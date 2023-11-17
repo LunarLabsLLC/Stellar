898a02a024154ceb12cb2e759644644f2fc23df9
Nopox <78834703+Nopock@users.noreply.github.com>
2023-11-16 20:42:50 -0800

    shit is broken

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { ClientPartials, IntentOptions } from "./config/ClientOptions";
-import StellarClient from "./client/StellarClient";
-import { registerInteractions } from "./client/interfaces";
-import path from "path";
+import path from 'node:path';
+import StellarClient from './client/StellarClient';
+import { registerInteractions } from './client/interfaces';
+import { ClientPartials, IntentOptions } from './config/ClientOptions';
 
-console.log("Starting the discord bot...")
+console.log('Starting the discord bot...');
 
 const client = new StellarClient({
-    intents: IntentOptions,
-    partials: ClientPartials,
-    allowedMentions: { parse: ["users"], repliedUser: true },
-    presence: {
-        status: "dnd"
-    }
-})
+	intents: IntentOptions,
+	partials: ClientPartials,
+	allowedMentions: { parse: ['users'], repliedUser: true },
+	presence: {
+		status: 'dnd',
+	},
+});
 
-client.registerCommands()
-client.registerEvents()
-registerInteractions(client, path.join(__dirname, "interactions"))
+client.registerCommands();
+client.registerEvents();
+registerInteractions(client, path.join(__dirname, 'interactions'));
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,13 +1,15 @@
+import process from 'node:process';
+
 export const validateEnv = () => {
-    if (!process.env.BOT_TOKEN) {
-        console.warn("Missing `BOT_TOKEN` in .env")
-        return false
-    }
+	if (!process.env.BOT_TOKEN) {
+		console.warn('Missing `BOT_TOKEN` in .env');
+		return false;
+	}
 
-    if (!process.env.MONGO_URI) {
-        console.warn("Missing `MONGO_URI` in .env")
-        return false
-    }
+	if (!process.env.MONGO_URI) {
+		console.warn('Missing `MONGO_URI` in .env');
+		return false;
+	}
 
-    return true
-}
+	return true;
+};
