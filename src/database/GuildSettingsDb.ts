241b5f1466426b2120632807b3ad27f7a98d0f8b
egirldevs <emily@Bunni.me>
2023-11-18 00:17:45 -0500

    GuildSetting Impl

---
+++ src/database/GuildSettingsDb.ts
@@ -0,0 +1,32 @@
+import Keyv from 'keyv';
+import KeyvMongo from '@keyv/mongo';
+import { LogService } from "../service/LogService";
+
+class GuildSettingsDb {
+  private keyv: Keyv;
+
+  constructor() {
+    const uri = 'mongodb://user:pass@localhost:27017/dbname';
+    const collection = 'guildSettings';
+
+    const keyv = new Keyv(uri, { store: new KeyvMongo(uri, { collection }) });
+
+    keyv.on('error', err => console.error('Connection Error', err));
+
+    this.keyv = keyv;
+  }
+
+  async setSetting(guildId: string, setting: string, value: any): Promise<void> {
+    await this.keyv.set(`${guildId}:${setting}`, value);
+  }
+
+  async getSetting(guildId: string, setting: string): Promise<any> {
+    return await this.keyv.get(`${guildId}:${setting}`);
+  }
+
+  async deleteSetting(guildId: string, setting: string): Promise<boolean> {
+    return await this.keyv.delete(`${guildId}:${setting}`);
+  }
+}
+
+export default GuildSettingsDb;
